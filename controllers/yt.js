ae43453ece12670a08f0de0b50eb65bb4980348c
wannofc <93016847+wannofc@users.noreply.github.com>
2022-03-25 15:31:09 +0700

    Add files via upload

---
+++ controllers/yt.js
@@ -0,0 +1,91 @@
+const {
+    ytDonlodMp3,
+    ytDonlodMp4,
+    ytPlayMp3,
+    ytPlayMp4,
+    ytSearch
+} = require("../lib/yt");
+const { cekKey, limitAdd, isLimit } = require('../database/db');
+
+async function ytSearch(req, res) {
+    const query = req.query.query;
+    const apikey = req.query.apikey;
+    if (query === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter query & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    let limit = await isLimit(apikey);
+    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
+    ytPlay(query).then(result => {
+        limitAdd(apikey);
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+async function ytPlayMp3(req, res) {
+    const url = req.query.url;
+    const apikey = req.query.apikey;
+    if (url === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    let limit = await isLimit(apikey);
+    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
+    ytMp3(url).then(result => {
+        limitAdd(apikey);
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+async function ytPlayMp4(req, res) {
+    const url = req.query.url;
+    const apikey = req.query.apikey;
+    if (url === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    let limit = await isLimit(apikey);
+    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
+    ytMp4(url).then(result => {
+        limitAdd(apikey);
+        res.status(200).send({
+            status: 200, 
+            result: result
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+module.exports = { ytSearch, ytPlayMp3, ytPlayMp4 };
